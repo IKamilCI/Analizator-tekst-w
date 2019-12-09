ab2665b5f85189310834e0e870cb5eebb4747b15
KamilC <54148375+IKamilCI@users.noreply.github.com>
2019-12-09 19:07:07 +0100

    Added main menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace AnalizatorTekstow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            while (true)
+            {
+                Console.WriteLine("1. Pobierz plik z internetu.");
+                Console.WriteLine("2. Zlicz liczbę liter w pobranym pliku.");
+                Console.WriteLine("3. Zlicz liczbę wyrazów w pliku.");
+                Console.WriteLine("4. Zlicz liczbę znaków interpunkcyjnych w pliku.");
+                Console.WriteLine("5. Zlicz liczbę zdań w pliku.");
+                Console.WriteLine("6. Wygeneruj raport o użyciu liter(A - Z).");
+                Console.WriteLine("7. Zapisz statystyki z punktów 2 - 5 do pliku statystyki.txt.");
+                Console.WriteLine("8. Wyjście z programu.");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 8)
+                    break;
+            }
         }
     }
 }
