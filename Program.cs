036df18705b449cb98359010de3e35798752b65f
KamilC <54148375+IKamilCI@users.noreply.github.com>
2019-12-12 21:36:43 +0100

    Added letters counter and esthetics

--- Program.cs
+++ Program.cs
@@ -8,29 +8,33 @@ namespace AnalizatorTekstow
     {
         static void Main(string[] args)
         {
-            string _url = "https://s3.zylowski.net/public/input/1.txt";// string with hardcoded url adress of the file
-            string _path = @"C:\\Users\\nazwa\\Desktop\\1.txt";//string with path to where save the file
+            string _url = "https://s3.zylowski.net/public/input/3.txt";// string with hardcoded url adress of the file
+            string _path = @"C:\Users\kamil\Desktop\szkola\Analizator-tekstow\zadanie.txt";//string with path to where save the file
             while (true)
             {
-                Console.WriteLine("1. Pobierz plik z internetu.");
+                Console.WriteLine("\n1. Pobierz plik z internetu.");
                 Console.WriteLine("2. Zlicz liczbę liter w pobranym pliku.");
                 Console.WriteLine("3. Zlicz liczbę wyrazów w pliku.");              
                 Console.WriteLine("4. Zlicz liczbę znaków interpunkcyjnych w pliku.");
                 Console.WriteLine("5. Zlicz liczbę zdań w pliku.");
                 Console.WriteLine("6. Wygeneruj raport o użyciu liter(A - Z).");
                 Console.WriteLine("7. Zapisz statystyki z punktów 2 - 5 do pliku statystyki.txt.");
-                Console.WriteLine("8. Wyjście z programu.");
+                Console.WriteLine("8. Wyjście z programu.\n");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
-                if (menuOption == 8)
-                    break;
-                if (menuOption == 3)
-                {
-                    Console.WriteLine(Counter(_path));
-                }
                 if (menuOption == 1)
                 {
                     Console.WriteLine(Downloader(_url, _path));                                             
                 }
+                if (menuOption == 2)
+                {
+                    Console.WriteLine(LettersCounter(_path));
+                }
+                if (menuOption == 3)
+                {
+                    Console.WriteLine(WordsCounter(_path));
+                }
+                if (menuOption == 8)
+                    break;
             }
 
             //metod downloading file from url, and saving it in the directory path
@@ -39,7 +43,7 @@ namespace AnalizatorTekstow
                 //checking if path and url are not empty to not couse exception
                 if(string.IsNullOrEmpty(url)&&string.IsNullOrEmpty(path))
                 {
-                    return "Adres url i/albo ścieżka są puste. Pobieranie nieudane.";
+                    return "\nAdres url i/albo ścieżka są puste. Pobieranie nieudane.";
                 }
                 else
                 {                    
@@ -47,28 +51,47 @@ namespace AnalizatorTekstow
                     {
                         myClient.DownloadFile(url, path);
                     }
-                    return "Pobieranie pliku rozpoczęte.";
+                    return "\nPlik został pobrany.";
                 }
               
             }
 
             //counting words in file 
-            string Counter(string path)
+            string LettersCounter(string path)
             {
                 if (File.Exists(path))
                 {
-                    int count;
+                    int lettersCount;
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        string tekst = File.ReadAllText(path);
+                        lettersCount = tekst.Length;
+                    }
+                    return "\nLiczba liter: " + lettersCount.ToString();
+                }
+                else
+                {
+                    return "\nBłąd, plik nie istnieje!";
+                }
+            }
+
+            //counting words in file 
+            string WordsCounter(string path)
+            {
+                if (File.Exists(path))
+                {
+                    int wordsCount;
                     using (StreamReader reader = new StreamReader(path))
                     {
                         string tekst = File.ReadAllText(path);
                         char[] rozdzielacze = new char[] { ' ', '\n', '\r' };
-                        count = tekst.Split(rozdzielacze, StringSplitOptions.RemoveEmptyEntries).Length;
+                        wordsCount = tekst.Split(rozdzielacze, StringSplitOptions.RemoveEmptyEntries).Length;
                     }
-                    return count.ToString();
+                    return "\nLiczba wyrazów: "+wordsCount.ToString();
                 }
                 else
                 {
-                    return "Błąd, plik nie istnieje!";
+                    return "\nBłąd, plik nie istnieje!";
                 }
             }
         }
